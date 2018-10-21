92c0d2ab6362543c0424dc807beb62f2a9b594ed
Bruce Gordon <brucegne@gmail.com>
2018-10-21 12:45:14 -0500

    netlify;

--- admin/admin.controller.js
+++ admin/admin.controller.js
@@ -9,7 +9,7 @@ app.controller('AdminCtrl', ['$scope', 'CommonProp', '$firebaseArray',
 	$scope.createPost = function(){
 		if($scope.data.newPostID){
 			var ref = firebase.database().ref();
-		  blogref = ref.child('blog/posts');
+		    blogref = ref.child('blog/posts');
 			var adminPostsArr = $firebaseArray(blogref);
 			adminPostsArr.$add({
 				postID: $scope.data.newPostID,
@@ -25,16 +25,25 @@ app.controller('AdminCtrl', ['$scope', 'CommonProp', '$firebaseArray',
 
 app.controller('EditCtrl', ['$scope','CommonProp', '$firebaseObject', function($scope,CommonProp, $firebaseObject){
 //	var ref = new Firebase('https://brucegne.firebaseio.com/blog/posts/');
-var ref = firebase.database().ref();
-blogref = ref.child('blog/posts');
+    var ref = firebase.database().ref();
+    blogref = ref.child('blog/posts');
     var postObj = $firebaseObject(blogref.child($scope.$stateParams.postKey));
     postObj.$bindTo($scope, 'post');
-		$scope.namer = CommonProp.getUser();
+	$scope.namer = CommonProp.getUser();
 }])
 
 app.controller('basisCtrl', function($scope, $http) {
-  $http.get("https://wt-9bf94d1b39dd624e4ca77a2e9249d4f0-0.sandbox.auth0-extend.com/emongo?location=Carleton").then(function (response) {
+  $http.get("http://wt-9bf94d1b39dd624e4ca77a2e9249d4f0-0.sandbox.auth0-extend.com/emongo?commodity=corn").then(function (response) {
       $scope.myData = response.data;
   });
+
+
+  $http.get("http:hfpaginfo.com/summbyrselect").then(function (response) {
+      $scope.mybuyers = response.data;
+  });
+
+    $scope.orderByMe = function(x) {
+        $scope.myOrderBy = x;
+      }
 });
 
